58ed140700c18ca22992585c94b24fa9205a58ce
tracer-ics2021 <tracer@njuics.org>
2023-01-11 21:41:31 -0800

    >  compile 3019244266    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 9ccabcf043e243618e1d3b8115300001            Boot ID: 4439f3fd741641c794092e6b9665ae6c     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  21:41:31 up 46 min,  1 user,  load average: 0.29, 0.18, 0.35

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -1,3 +1,10 @@
+/*
+ * @Author: Runze Li lirunze.me@gmail.com
+ * @Date: 2023-01-11 02:09:44
+ * @LastEditors: Runze Li
+ * @LastEditTime: 2023-01-11 21:41:24
+ * @Description:  
+ */
 #include <utils.h>
 
 NEMUState nemu_state = { .state = NEMU_STOP };
@@ -5,5 +12,6 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-  return !good;
+  // return !good;
+  return good;
 }
