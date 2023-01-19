aab7830f9205956f0caa252dc7604541fa461778
Runze Li <lirunze.me@gmail.com>
2023-01-18 20:31:39 -0800

    update decode operand helper function

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -2,7 +2,7 @@
  * @Author: Runze Li lirunze.me@gmail.com
  * @Date: 2023-01-11 02:09:44
  * @LastEditors: Runze Li
- * @LastEditTime: 2023-01-17 22:34:38
+ * @LastEditTime: 2023-01-18 20:27:32
  * @Description:  
  */
 #include "../local-include/reg.h"
@@ -29,7 +29,21 @@ static def_DopHelper(r) {
   op->preg = (is_write && val == 0) ? &zero_null : &gpr(val);
 }
 
-// TODO: 不知道需不需要添加R-type
+static def_DopHelper(j) {
+  op->imm = 0;
+  op->imm |= (s->isa.instr.j.imm10_1  << 1);
+  op->imm |= (s->isa.instr.j.imm11    << 11);
+  op->imm |= (s->isa.instr.j.imm19_12 << 12);
+  op->imm |= (s->isa.instr.j.simm20   << 20);
+}
+
+// TODO: R-type, B-type and U-type
+
+static def_DHelper(R) {
+  decode_op_r(s, id_src1, s->isa.instr.r.rs1, false);
+  decode_op_r(s, id_src2, s->isa.instr.r.rs2, false);
+  decode_op_r(s, id_dest, s->isa.instr.r.rd, true);
+}
 
 static def_DHelper(I) {
   decode_op_r(s, id_src1, s->isa.instr.i.rs1, false);
@@ -37,16 +51,32 @@ static def_DHelper(I) {
   decode_op_r(s, id_dest, s->isa.instr.i.rd, true);
 }
 
+static def_DHelper(S) {
+  decode_op_r(s, id_src1, s->isa.instr.s.rs1, false);
+  sword_t simm = (s->isa.instr.s.simm11_5 << 5) 
+               | (s->isa.instr.s.imm4_0);
+  decode_op_i(s, id_src2, simm, false);
+  decode_op_r(s, id_dest, s->isa.instr.s.rs2, false);
+}
+
+static def_DHelper(B) {
+  decode_op_r(s, id_src1, s->isa.instr.b.rs1, false);
+  sword_t simm = (s->isa.instr.b.simm12   << 12) 
+               | (s->isa.instr.b.imm11    << 11) 
+               | (s->isa.instr.b.imm10_5  << 5) 
+               | (s->isa.instr.b.imm4_1   << 1);
+  decode_op_i(s, id_src2, simm, false);
+  decode_op_r(s, id_dest, s->isa.instr.b.rs2, false);
+}
+
 static def_DHelper(U) {
   decode_op_i(s, id_src1, s->isa.instr.u.imm31_12 << 12, true);
   decode_op_r(s, id_dest, s->isa.instr.u.rd, true);
 }
 
-static def_DHelper(S) {
-  decode_op_r(s, id_src1, s->isa.instr.s.rs1, false);
-  sword_t simm = (s->isa.instr.s.simm11_5 << 5) | s->isa.instr.s.imm4_0;
-  decode_op_i(s, id_src2, simm, false);
-  decode_op_r(s, id_dest, s->isa.instr.s.rs2, false);
+static def_DHelper(J) {
+  decode_op_j(s, id_src1, 0, false);
+  decode_op_r(s, id_dest, s->isa.instr.j.rd, true);
 }
 
 def_THelper(load) {
