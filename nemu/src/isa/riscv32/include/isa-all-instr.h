d7b517aa200dd13a75e079acb03f7059846c9363
tracer-ics2021 <tracer@njuics.org>
2023-01-18 23:09:10 -0800

    >  compile 3019244266    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 9ccabcf043e243618e1d3b8115300001            Boot ID: 6acef408b6ca41599e7e371c73495d89     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-58-generic       Architecture: x86-64  23:09:11 up  2:39,  1 user,  load average: 1.38, 1.09, 1.09

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,12 +2,16 @@
  * @Author: Runze Li lirunze.me@gmail.com
  * @Date: 2023-01-11 02:09:44
  * @LastEditors: Runze Li
- * @LastEditTime: 2023-01-18 22:47:29
+ * @LastEditTime: 2023-01-18 22:55:59
  * @Description:  
  */
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(addi) f(inv) f(nemu_trap)
+#define INSTR_LIST(f) f(lui) f(auipc) \
+                      f(lw) \
+                      f(sw) \
+                      f(addi) \
+                      f(inv) f(nemu_trap)
 
 def_all_EXEC_ID();
