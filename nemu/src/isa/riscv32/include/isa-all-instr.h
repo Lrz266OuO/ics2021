348e50e704322c4c8aa6a25fa3b1fb44635f5204
tracer-ics2021 <tracer@njuics.org>
2023-01-19 20:16:48 -0800

    >  compile 3019244266    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 9ccabcf043e243618e1d3b8115300001            Boot ID: 989f8a91211b473fb2557310dc30e781     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-58-generic       Architecture: x86-64  20:16:48 up 52 min,  1 user,  load average: 0.58, 0.58, 0.55

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,14 +2,14 @@
  * @Author: Runze Li lirunze.me@gmail.com
  * @Date: 2023-01-11 02:09:44
  * @LastEditors: Runze Li
- * @LastEditTime: 2023-01-18 23:10:19
+ * @LastEditTime: 2023-01-19 20:00:00
  * @Description:  
  */
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(lui) f(auipc) \
-                      f(jal) \
+                      f(jal) f(jalr) \
                       f(lw) \
                       f(sw) \
                       f(addi) \
--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -2,7 +2,7 @@
  * @Author: Runze Li lirunze.me@gmail.com
  * @Date: 2023-01-18 23:43:00
  * @LastEditors: Runze Li
- * @LastEditTime: 2023-01-19 19:38:57
+ * @LastEditTime: 2023-01-19 20:16:37
  * @Description:  
  */
 
@@ -10,4 +10,11 @@ def_EHelper(jal) {
   rtl_addi(s, ddest, &s->pc, 4);
   rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm);
   // stack_call(s->pc, s->dnpc);
+}
+
+def_EHelper(jalr) {
+  rtl_addi(s, s0, dsrc1, id_src2->imm);
+  rtl_andi(s, &s->dnpc, s0, ~1);
+  rtl_addi(s, ddest, &s->pc, 4);
+  // stack_?
 }
