683ca26ab1834435bbddfbc017b4361e72f9a64b
tracer-ics2021 <tracer@njuics.org>
2023-01-19 23:28:22 -0800

    >  compile 3019244266    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 9ccabcf043e243618e1d3b8115300001            Boot ID: 989f8a91211b473fb2557310dc30e781     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-58-generic       Architecture: x86-64  23:28:22 up  2:51,  1 user,  load average: 0.91, 1.01, 0.86

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -2,15 +2,14 @@
  * @Author: Runze Li lirunze.me@gmail.com
  * @Date: 2023-01-18 23:43:00
  * @LastEditors: Runze Li
- * @LastEditTime: 2023-01-19 23:26:53
+ * @LastEditTime: 2023-01-19 23:28:17
  * @Description:  
  */
 
 def_EHelper(jal) {
   rtl_addi(s, ddest, &s->pc, 4);
-  // rtl_addi(s, s0, &s->pc, id_src1->imm);
-  // rtl_j(s, *s0);
-  rtl_j(s, *((&s->pc) + (id_src1->imm)));
+  rtl_addi(s, s0, &s->pc, id_src1->imm);
+  rtl_j(s, *s0);
   /* TODO: The standard software calling convention 
    * uses x1 as the return address register 
    * and x5 as an alternate link register.
