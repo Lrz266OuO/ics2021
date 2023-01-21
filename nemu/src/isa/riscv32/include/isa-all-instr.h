868d7dc71e6e79d3dd58b57738a824bf21a4ba1e
tracer-ics2021 <tracer@njuics.org>
2023-01-21 01:59:56 -0800

    >  compile 3019244266    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 9ccabcf043e243618e1d3b8115300001            Boot ID: 04759f0f88b344d98eae5fc7b69c82e9     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-58-generic       Architecture: x86-64  01:59:57 up  1:00,  1 user,  load average: 0.79, 0.61, 0.47

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,7 +2,7 @@
  * @Author: Runze Li lirunze.me@gmail.com
  * @Date: 2023-01-11 02:09:44
  * @LastEditors: Runze Li
- * @LastEditTime: 2023-01-21 01:56:25
+ * @LastEditTime: 2023-01-21 01:58:32
  * @Description:  
  */
 #include <cpu/decode.h>
@@ -13,7 +13,7 @@
                       f(beq) f(bne) \
                       f(lw) f(lbu) \
                       f(sh) f(sw) \
-                      f(addi) f(sltiu) f(andi) f(srai) \
+                      f(addi) f(sltiu) f(xori) f(andi) f(srai) \
                       f(add) f(sub) f(sll) f(sltu) f(xor) f(or) f(and) \
                       f(inv) f(nemu_trap)
 
