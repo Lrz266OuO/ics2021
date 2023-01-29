87f5872c348f9b039daa07965af59dba2e36cd6c
Runze Li <lirunze.me@gmail.com>
2023-01-29 05:11:55 -0800

    add clock and keyborad but I don't know how to test them

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,20 @@
+/*
+ * @Author: Runze Li lirunze.me@gmail.com
+ * @Date: 2023-01-17 02:29:32
+ * @LastEditors: Runze Li
+ * @LastEditTime: 2023-01-29 05:10:38
+ * @Description:  
+ */
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t keycode = inl(KBD_ADDR);
+  // kbd->keydown = 0;
+  // kbd->keycode = AM_KEY_NONE;
+  kbd->keydown = keycode & KEYDOWN_MASK;
+  kbd->keycode = keycode & (~(KEYDOWN_MASK));
 }
